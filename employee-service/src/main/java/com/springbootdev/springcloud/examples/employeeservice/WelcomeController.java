71022084cdcd4f6877e9b63f73e64a389772c711
apaper <apaper@yahoo.com>
2020-05-14 17:21:10 -0400

    Add vault shared and private properties integration with new endpoints to test along with the postContruct to debug

--- employee-service/src/main/java/com/springbootdev/springcloud/examples/employeeservice/WelcomeController.java
+++ employee-service/src/main/java/com/springbootdev/springcloud/examples/employeeservice/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.springbootdev.springcloud.examples.employeeservice;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +13,35 @@ public class WelcomeController
 {
     @Value("${app.service-name}")
     private String serviceName;
+    
+    @Value("${shared.pseudo.property}")
+    private String sharedProperty;
+    
+    @Value("${private.pseudo.property}")
+    private String privateProperty;
 
     @GetMapping("/service")
     public String getServiceName() {
         return "service name [" + this.serviceName + "]";
     }
+    
+    @GetMapping("/service/shared/property")
+    public String getSharedProperty() {
+        return "shared property [shared.pseudo.property=" + this.sharedProperty + "]";
+    }
+    
+    @GetMapping("/service/private/property")
+    public String getPrivateProperty() {
+        return "private property [private.pseudo.property=" + this.privateProperty + "]";
+    }
+    
+	@PostConstruct
+	private void postConstruct() {
+		System.out.println("##########################");
+		System.out.println(getServiceName());
+		System.out.println(getSharedProperty());
+		System.out.println(getPrivateProperty());
+		System.out.println("##########################");
+	}
+    
 }
