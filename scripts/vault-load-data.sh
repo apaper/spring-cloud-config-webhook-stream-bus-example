cb7a4701970f79cd34f812e3adefd65c26cca520
apaper <apaper@yahoo.com>
2020-05-14 17:06:49 -0400

    Update vault data to illustrate concept of shared and private application properties

--- scripts/vault-load-data.sh
+++ scripts/vault-load-data.sh
@@ -9,8 +9,8 @@ export VAULT_TOKEN='myroot'
 
 vault secrets list
 vault kv put secret/application shared.pseudo.property="shared property value loaded from vault for application"
-vault kv put secret/department-service client.pseudo.property="private property value loaded from vault for department-service"
-vault kv put secret/employee-service client.pseudo.property="private property value loaded from vault for employee-service"
+vault kv put secret/department-service private.pseudo.property="private property value loaded from vault for department-service"
+vault kv put secret/employee-service private.pseudo.property="private property value loaded from vault for employee-service"
 vault kv get secret/application
 vault kv get secret/department-service
 vault kv get secret/employee-service
