f3b1ea069b4151adc3bacb1ae0daacab6106773d
apaper <apaper@yahoo.com>
2020-07-17 14:32:29 -0400

    []]

--- scripts/clean-logs.sh
+++ scripts/clean-logs.sh
@@ -6,13 +6,16 @@ echo "*************************************"
 echo " list the log files"
 echo "*************************************"
 find . -type f -name "*.log" -print
+find . -type f -name "*.log.*.gz" -print
 echo "*************************************"
 echo " delete the log files"
 echo "*************************************"
 find . -type f -name "*.log" -delete
+find . -type f -name "*.log.*.gz" -delete
 echo "*************************************"
 echo " list the log files"
 echo "*************************************"
 find . -type f -name "*.log" -print
+find . -type f -name "*.log.*.gz" -print
 
 popd > /dev/null
