53532d859f0555b1c8e9739dd56b596f3195dad9
apaper <apaper@yahoo.com>
2020-07-28 15:00:33 -0400

    Update data.sql

--- employee-service/src/main/resources/data.sql
+++ employee-service/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO 
-	TBL_EMPLOYEES (first_name, last_name, email) 
+INSERT INTO
+     TBL_EMPLOYEES (first_name, last_name, email) 
 VALUES
-  	('Lokesh', 'Gupta', 'howtodoinjava@gmail.com'),
-  	('John', 'Doe', 'xyz@email.com');
+     ('Bruce', 'Wayne', 'batman@gmail.com'),
+     ('Clark', 'Kent', 'superman@gmail.com');
