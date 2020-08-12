7eac61b1f72fc9cbccd98345f49ab0d0c679dca0
apaper <apaper@yahoo.com>
2020-08-12 08:23:21 -0400

    vault-load-data.sh

--- scripts/vault-load-data.sh
+++ scripts/vault-load-data.sh
@@ -10,10 +10,10 @@ export VAULT_TOKEN='myroot'
 vault secrets list
 vault kv put secret/application shared.pseudo.property="shared property value loaded from vault for application"
 vault kv put secret/department-service private.pseudo.property="private property value loaded from vault for department-service"
-#vault kv put secret/scim-service private.pseudo.property="private property value loaded from vault for scim-service" \ private.db.username="postgres" \ private.db.password="password"
-vault kv put -format=json secret/scim-service @scim-service.json
-#vault kv put secret/employee-service private.pseudo.property="private property value loaded from vault for employee-service" \ private.db.username="employee-service" \ private.db.password="employee-service-password"
-vault kv put -format=json secret/employee-service @employee-service.json
+vault kv put secret/scim-service private.pseudo.property="private property value loaded from vault for scim-service" \ private.db.username="postgres" \ private.db.password="password"
+#vault kv put -format=json secret/scim-service @scim-service.json
+vault kv put secret/employee-service private.pseudo.property="private property value loaded from vault for employee-service" \ private.db.username="employee-service" \ private.db.password="employee-service-password"
+#vault kv put -format=json secret/employee-service @employee-service.json
 vault kv put secret/eureka-service private.pseudo.property="private property value loaded from vault for eureka-service"
 vault kv put secret/zuul-service private.pseudo.property="private property value loaded from vault for zuul-service"
 vault kv get secret/application
